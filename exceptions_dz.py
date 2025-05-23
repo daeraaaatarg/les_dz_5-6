fc450b0a4e47d3d6458b18e7c696031ae810f8e3
ACER <kutova.anastasia00@gmail.com>
2025-05-23 16:53:25 +0300

    mistakes

--- exceptions_dz.py
+++ exceptions_dz.py
@@ -10,7 +10,7 @@
 #data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
 #
 #for key in data:
-#    res = divider(key, data[key])
+#    res = divider(key, data[kem])
 #    result.append(res)
 #
 #print(result)
