6d0b90195cc103a56b11fe21d060cfcf8998ca99
ACER <kutova.anastasia00@gmail.com>
2025-05-23 16:44:25 +0300

    інтроспекція

---
+++ introspection.py
@@ -0,0 +1,14 @@
+import colorama
+# pressed Ctrl
+
+# the result:
+
+#from .ansi import Back as Back, Cursor as Cursor, Fore as Fore, Style as Style  - фон, курсор, колір, стиль кольори
+#from .ansitowin32 import AnsiToWin32 as AnsiToWin32
+#from .initialise import (
+#    colorama_text as colorama_text,
+#    deinit as deinit,
+#    init as init,
+#    just_fix_windows_console as just_fix_windows_console,
+#    reinit as reinit,
+#)
